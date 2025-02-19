db14f9175c65244f6c53afc564007eed070a8139
ReekyStive <reekystive@gmail.com>
2025-02-19 13:35:52 +0800

    fix: 🐞 prevent default touch behavior in knob interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,15 +43,29 @@ const Home: FC = () => {
       <h1 className={`text-6xl font-bold mb-8 text-center opacity-90 ${qwigley.className}`}>4xOsc Web Synth</h1>
       <div className="max-w-4xl flex flex-col items-center gap-8">
         <div className="flex flex-row items-center gap-8">
-          <div className="grid w-fit grid-cols-2 gap-4">
-            {([1, 2, 3, 4] as const).map((number) => (
-              <Oscillator
-                key={number}
-                number={number}
-                params={oscillators[`osc${number}` as keyof typeof oscillators]}
-                onParamsChange={(params) => handleOscParamsChange(number, params)}
-              />
-            ))}
+          <div className="flex flex-col gap-4">
+            <div className="grid w-fit grid-cols-2 gap-4">
+              {([1, 2, 3, 4] as const).map((number) => (
+                <Oscillator
+                  key={number}
+                  number={number}
+                  params={oscillators[`osc${number}` as keyof typeof oscillators]}
+                  onParamsChange={(params) => handleOscParamsChange(number, params)}
+                />
+              ))}
+            </div>
+            <div
+              className="text-gray-400 border-slate-500 opacity-20 text-xl
+              ml-[30px] border-2 h-[180px] rounded-md flex items-center justify-center"
+            >
+              LFO Planned
+            </div>
+            <div className={`text-gray-400 opacity-20 text-4xl ml-[30px] ${qwigley.className}`}>
+              Made by{' '}
+              <a href="https://github.com/reekystive/4xosc-web-synth" target="_blank" className="text-gray-200">
+                reekystive
+              </a>
+            </div>
           </div>
           <div className="flex flex-col gap-4">
             <Envelope
